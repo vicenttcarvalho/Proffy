f2d579fc283aab63197e60c12193ffbfb42d81aa
vicenttcarvalho <vicentecleyton@gmail.com>
2020-08-15 17:19:17 -0300

    Rotas recursos métodos HTTP e parâmetros

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json()); //para o app entender o arquivo json
+
+app.get('/users', (request, response) => {
+    console.log(request.query);
+
+    const users = [
+        { name: 'Diego', age: 25 },
+        { name: 'Vini', age: 21 },
+    ];
+    return response.json(users);
+});
+
+app.listen(3333); //ouvir requisições HTTP
