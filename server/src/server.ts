5f84109f6d7607637528a5435483e633e957dd5e
vicenttcarvalho <vicentecleyton@gmail.com>
2020-08-15 17:25:37 -0300

    Configurando conexão com o banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.use(express.json()); //para o app entender o arquivo json
+app.use(express.json());
 
-app.get('/users', (request, response) => {
-    console.log(request.query);
-
-    const users = [
-        { name: 'Diego', age: 25 },
-        { name: 'Vini', age: 21 },
-    ];
-    return response.json(users);
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World' });
 });
 
-app.listen(3333); //ouvir requisições HTTP
+app.listen(3333);
